1111ba8a2b8831f376177c682b9b0ea32c005ffc
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-20 23:29:22 +0300

    third

---
+++ third/third/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            
+            data = Console.ReadLine();
+
+            Console.WriteLine("Привет " + data + "!!!");
+        }
+    }
+}
