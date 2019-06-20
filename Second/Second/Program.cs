a06d5097853526da852747c1cf3ca9db605b9f7c
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-20 23:23:06 +0300

    second

---
+++ Second/Second/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b = 6;
+
+            a = 5;
+
+            Console.Write(a);
+            Console.WriteLine(b);
+
+            bool variable = false;
+            bool Variable = true;
+
+            Console.WriteLine(variable);
+            Console.WriteLine(Variable);
+
+            double aa = 1.33;
+
+            char aaa = '#';
+
+            string bb = "dfgsg2";
+
+            Console.WriteLine(aaa);
+            Console.WriteLine(bb);
+
+            a = 33;
+
+            Console.WriteLine(a);
+        }
+    }
+}
