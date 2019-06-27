efc7b98126c3dd7392ff33d70fcd9b8ec3e7101f
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-27 23:49:36 +0300

    operators

---
+++ operators/operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 5;
+            double result = a / b;
+            Console.WriteLine(result); //0
+
+            double c = 2;
+            result = c / b;
+            Console.WriteLine(result); //0.4
+
+            result = (double)a / b;
+            Console.WriteLine(result); //0.4
+
+
+            a = 10;
+            b = 3;
+            result = a % b;
+            Console.WriteLine(result);
+        }
+    }
+}
