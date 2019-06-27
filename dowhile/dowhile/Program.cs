81efe76f1bedf1d5f903da201ce4d09cf423cbaa
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 02:57:20 +0300

    dowhile

---
+++ dowhile/dowhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            do
+            {
+                count++;
+                Console.WriteLine(count);
+            } while (count < 5);
+            Console.ReadLine();
+        }
+    }
+}
