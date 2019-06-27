9a8db2522f66c4d22d280be62c10c55dd9ba1b11
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 01:48:07 +0300

    logicoperators

---
+++ logicoperators/logicoperators/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicoperators
+{
+    class Program
+    {
+        public static bool GetTemperature()
+        {
+            return true;
+        }
+        public static bool GetCoolingStatus()
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            bool isInfected = false;
+
+            if (!isInfected)
+            {
+                Console.WriteLine("Персонаж здоров");
+            }
+
+
+            bool isHighTemperature = true;
+            bool hasNoCooling = false;
+
+            if (isHighTemperature && hasNoCooling)
+            {
+                Console.WriteLine("Угроза повреждения процессора!");
+            }
+            if (isHighTemperature || hasNoCooling)
+            {
+                Console.WriteLine("Небольшая угроза повреждения процессора");
+            }
+
+            //& и | проверяют все условия, даже если первые false
+            if(GetTemperature()  | GetCoolingStatus())
+            {
+                Console.WriteLine("Угроза повреждения процессора!");
+            }
+            if (GetTemperature() || GetCoolingStatus())
+            {
+                Console.WriteLine("Угроза повреждения процессора!");
+            }
+        }
+    }
+}
