299fa9b78ed68990acf7439208269d6fbd83c763
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 01:28:50 +0300

    dz2

---
+++ dz2/dz2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите число: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a + " - чётное");
+            }
+            else
+            {
+                Console.WriteLine(a + " - нечётное");
+            }
+        }
+    }
+}
