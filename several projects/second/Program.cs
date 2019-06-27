06db557cb3599940a6f003a3c123357eb6893828
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 00:02:13 +0300

    several projects

---
+++ several projects/second/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("second");
+            Console.ReadLine();
+        }
+    }
+}
