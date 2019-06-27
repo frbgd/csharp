ad7210239cb75f77061ba354a99cf51e8668c341
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 02:28:36 +0300

    dz3

---
+++ Calc/ifelse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Введите первый операнд: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Введите второй операнд: ");
+            b = double.Parse(Console.ReadLine());
+
+            string op;
+            Console.Write("Введите операцию: ");
+            op = Console.ReadLine();
+            if (op == "+")
+            {
+                Console.WriteLine("Результат сложения: " + (a + b));
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine("Результат вычитания: " + (a - b));
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine("Результат умножения: " + (a * b));
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine("Результат деления: " + (a / b));
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неизвестный символ");
+            }
+        }
+    }
+}
---
+++ Calc/switch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Введите первый операнд: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Введите второй операнд: ");
+            b = double.Parse(Console.ReadLine());
+
+            string op;
+            Console.Write("Введите операцию: ");
+            op = Console.ReadLine();
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine("Результат сложения: " + (a + b));
+                    break;
+                case "-":
+                    Console.WriteLine("Результат вычитания: " + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine("Результат умножения: " + (a * b));
+                    break;
+                case "/":
+                    Console.WriteLine("Результат деления: " + (a / b));
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели неизвестный символ");
+                    break;
+            }
+        }
+    }
+}
