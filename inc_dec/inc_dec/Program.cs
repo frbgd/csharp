6173dc5503960ab47be55ef4a8ea267cd9d64478
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 00:59:32 +0300

    inc_dec

---
+++ inc_dec/inc_dec/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inc_dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine(a++); //0
+            int b = 0;
+            Console.WriteLine(++b); //1
+
+            int c = 1;
+            c = ++c * c;
+            Console.WriteLine(c); //4
+
+            int d = 1;
+            d = d++ * d;
+            Console.WriteLine(d);
+        }
+    }
+}
---
+++ inc_dec/inc_dec/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("inc_dec")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("inc_dec")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22797bed-cf7e-4de4-9be0-e345f912b3e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
