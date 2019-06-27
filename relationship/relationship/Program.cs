28ac9a829e3d36ac3e718eebdf6ff2cf484b43f7
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 01:16:48 +0300

    relationship

---
+++ relationship/relationship/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace relationship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 5;
+            bool result = a == b;
+            Console.WriteLine(result);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a < b);
+        }
+    }
+}
