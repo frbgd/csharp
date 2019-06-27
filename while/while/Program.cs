9a24ebd404fb3dd6ada2c34354ec8c9e32aee3a4
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 02:51:23 +0300

    while

---
+++ while/while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint limit;
+            limit = uint.Parse(Console.ReadLine());
+            uint count = 0;
+            while (count < limit)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+            Console.ReadLine();
+        }
+    }
+}
