2bc139cd04797ba8c928e57b517b3afface92826
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 01:26:19 +0300

    ifelse

---
+++ ifelse/ifelse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a == 5)
+            {
+                Console.WriteLine("a равна 5");
+            }
+            else
+            {
+                Console.WriteLine("a не равна 5");
+            }
+        }
+    }
+}
