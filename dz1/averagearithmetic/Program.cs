b6e1bd4619f16dca64dc632b15eb7b612a798015
Алексей Кучерявенко <kap17597@gmail.com>
2019-06-28 00:16:27 +0300

    dz1-1

---
+++ dz1/averagearithmetic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace averagearithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            try
+            {
+                Console.Write("Введите первое число: ");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Среднее арифметическое " + a + " и " + b + " = " + (a + b) / 2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неверный формат ввода");
+            }
+            Console.WriteLine("Закрытие программы!");
+        }
+    }
+}
