856d53745ebad0a62bd6f06c584365dee501dc85
Алексей Кучерявенко <kap17597@gmail.com>
2019-07-15 20:44:14 +0300

    metanit-pr1.1.6

---
+++ metanit-pr1.1.6/metanit-pr1.1.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metanit_pr1._1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Сложение");
+                    break;
+                case 2:
+                    Console.WriteLine("Вычитание");
+                    break;
+                case 3:
+                    Console.WriteLine("Умножение");
+                    break;
+                default:
+                    Console.WriteLine("Операция не определена");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
