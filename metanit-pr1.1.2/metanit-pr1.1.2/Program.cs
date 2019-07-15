31010b69b58b4dc4d208960ce0adaa298251f276
Алексей Кучерявенко <kap17597@gmail.com>
2019-07-15 20:31:48 +0300

    metanit-pr1.1.2

---
+++ metanit-pr1.1.2/metanit-pr1.1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metanit_pr1._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a > 5 && a < 10)
+                Console.WriteLine("Число больше 5 и меньше 10");
+            else
+                Console.WriteLine("Неизвестное число");
+            Console.ReadKey();
+        }
+    }
+}
