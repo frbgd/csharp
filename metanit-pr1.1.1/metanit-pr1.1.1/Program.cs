9ca3725936d206109b9d98239e675d57017002cc
Алексей Кучерявенко <kap17597@gmail.com>
2019-07-15 20:29:12 +0300

    metanit-pr1.1.1

---
+++ metanit-pr1.1.1/metanit-pr1.1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metanit_pr1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if(a > b)
+                Console.WriteLine("a > b");
+            else if(a < b)
+                Console.WriteLine("a < b");
+            else
+                Console.WriteLine("a == b");
+            Console.ReadKey();
+        }
+    }
+}
