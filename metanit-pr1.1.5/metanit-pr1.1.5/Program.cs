e64f6dae2b3184274f0ed93cee07a6a8517f4f5d
Алексей Кучерявенко <kap17597@gmail.com>
2019-07-15 20:40:38 +0300

    metanit-pr.1.1.5

---
+++ metanit-pr1.1.5/metanit-pr1.1.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metanit_pr1._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum;
+            sum = Convert.ToDouble(Console.ReadLine());
+            if (sum < 100)
+                sum *= 1.05;
+            else if (sum < 200)
+                sum *= 1.07;
+            else
+                sum *= 1.1;
+            sum += 15;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
