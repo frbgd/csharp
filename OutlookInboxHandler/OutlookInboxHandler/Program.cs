cbdf15d145569b26cb2cf6d5b8828efd91e4f89d
Алексей Кучерявенко <kap17597@gmail.com>
2019-08-02 06:08:58 +0300

    work with Arbor

--- OutlookInboxHandler/OutlookInboxHandler/Program.cs
+++ OutlookInboxHandler/OutlookInboxHandler/Program.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Office.Interop.Outlook;
+﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using Microsoft.Office.Interop.Outlook;
 using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Collections.Generic;
@@ -33,18 +34,35 @@ namespace OutlookInboxHandler
                             while ((line = sr.ReadLine()) != null)
                             {
                                 string[] splitLine = line.Trim().Split(' ');
-                                if(Convert.ToInt32(splitLine[0]) >= 1000)
+                                if (Convert.ToInt32(splitLine[0]) >= 1000)
                                 {
                                     addresses.Add(splitLine[1]);
                                 }
                             }
                         }
-
-
                     }
-                    
                 }
+                else
+                    break;
             }
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://vpi1.soc.rt.ru/page?id=mitigation_status&mitigation_id=58640");
+            driver.FindElement(By.Name("username")).SendKeys("a.kucheryavenko");
+            driver.FindElement(By.Name("password")).SendKeys("4_c`&MjLjq");
+            driver.FindElement(By.Name("Submit")).Click();
+
+            driver.FindElement(By.CssSelector(".alt:nth-child(5) a"));
+            IWebElement filterForm = driver.FindElement(By.Name("filter_MitigationRealTimeExpandBWList_bcfea401019cccd2db81b44b4b11d7c9"));
+            string filter = filterForm.Text;
+            foreach (string address in addresses)
+            {
+                filter = $"drop src host {address}\n{filter}";
+            }
+            filterForm.SendKeys(filter);
+            driver.FindElement(By.CssSelector(".tableheader:nth-child(8) .tick"));
+
+            driver.FindElement(By.LinkText("Log Out")).Click();
         }
     }
 }
