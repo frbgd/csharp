0aa87f5075cebe7fb21e8f1472f8aea872190a63
Алексей Кучерявенко <kap17597@gmail.com>
2019-08-04 16:51:34 +0300

    errors in AddToFilterList

--- OutlookInboxHandler/OutlookInboxHandler/Program.cs
+++ OutlookInboxHandler/OutlookInboxHandler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using Microsoft.Office.Interop.Outlook;
 using System;
@@ -6,6 +7,7 @@ using System.Runtime.InteropServices;
 using System.IO;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 using MihaZupan;
@@ -14,20 +16,65 @@ namespace OutlookInboxHandler
 {
     class Program
     {
+        enum Browser
+        {
+            Firefox,
+            Chrome
+        }
+        static bool IsInvalidCount(string count)
+        {
+            try
+            {
+                if (Convert.ToInt32(count) < 0)
+                {
+                    return true;            //проверка на положительное значение
+                }
+            }
+            catch
+            {
+                return true;        //проверка на то, что количество адресов - число
+            }
+            return false;
+        }
+        static bool IsInvalidAddress(string[] address)
+        {
+            foreach(var octet in address)
+            {
+                try
+                {
+                    if (Convert.ToInt32(octet) < 0 || Convert.ToInt32(octet) > 255)
+                    {
+                        return true;        //проверка на валидность чисел в октетах
+                    }
+                }
+                catch
+                {
+                    return true;        //проверка на то, что октет - число
+                }
+            }
+            return false;
+        }
+
         static void GetAddressesFromOutlook(ref List<string> addresses)
         {
-            NameSpace NS = (Marshal.GetActiveObject("Outlook.Application") as Application).GetNamespace("MAPI");
-            Folder folder = (Folder)NS.Folders["frbgd7@mail.ru"].Folders["test"];
-            //Folder folder = (Folder)NS.Folders["soc@RT.RU"].Folders["Входящие"].Folders["ELK"];
+            Console.Write("Connecting to Outlook...");
+            NameSpace NS = (Marshal.GetActiveObject("Outlook.Application") as Application).GetNamespace("MAPI");    //здесь может выброситься ex.Source == "mscorlib"
+            Console.Write("Done\nSearching for folder \\\\soc@RT.RU\\ELK...");
+            //Folder folder = (Folder)NS.Folders["frbgd7@mail.ru"].Folders["test"];
+            Folder folder = (Folder)NS.Folders["soc@RT.RU"].Folders["Входящие"].Folders["ELK"];       //здесь может выброситься ex.Source == "Microsoft Outlook"
 
+            Console.WriteLine("Done\nMessages Scanning:");
+            int messageNumber = 0;
             foreach (MailItem mailItem in folder.Items)
             {
                 if (mailItem.ReceivedTime.Hour == DateTime.Now.Hour)
                 {
+                    Console.WriteLine($"Message: {++messageNumber}");
                     if (mailItem.Attachments.Count > 0)
                     {
                         foreach (Attachment txt in mailItem.Attachments)
                         {
+                            Console.Write($"Saving attachment in the file C:\\ELK\\{mailItem.ConversationTopic}_{DateTime.Now.ToString("yyyy-MM-dd HH")}h.txt...");
                             if (!Directory.Exists("C:\\ELK"))
                             {
                                 Directory.CreateDirectory("C:\\ELK");
@@ -35,13 +82,14 @@ namespace OutlookInboxHandler
                             var path = $"C:\\ELK\\{mailItem.ConversationTopic}_{DateTime.Now.ToString("yyyy-MM-dd HH")}h.txt";
                             txt.SaveAsFile(path);
 
+                            Console.Write($"Done\nReading file {mailItem.ConversationTopic}_{DateTime.Now.ToString("yyyy-MM-dd HH")}h.txt...");
                             using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
                             {
                                 string line;
                                 while ((line = sr.ReadLine()) != null)
                                 {
                                     string[] splitLine = line.Trim().Split(' ');
-                                    if(splitLine.Count() != 2)      //если неверный формат строки (файла), переходим к следующему файлу - добавить в уведомление
+                                    if(splitLine.Count() != 2 || IsInvalidCount(splitLine[0]) || splitLine[1].Split('.').Count() != 4 || IsInvalidAddress(splitLine[1].Split('.')))      //если неверный формат строки (файла), переходим к следующему файлу - добавить в уведомление
                                     {
                                         break;
                                     }
@@ -51,37 +99,53 @@ namespace OutlookInboxHandler
                                     }
                                 }
                             }
+                            Console.WriteLine("Done\nNext file.");
                         }
                     }
-                    else            //если в письме нет вложений - добавить в уведомление
+                    else            //если в письме нет вложений, переходим к следующему письму - добавить в уведомление
                     {
-
+                        Console.Write("Message have not attachments.\t");
                     }
                     try
                     {
                         mailItem.Move(folder.Folders["Done"]);
+                        Console.Write("Message moved to \\\\soc@RT.RU\\ELK\\Done.\t");
                     }
-                    catch       //не существует папка для перемещения - добавить в уведомление
+                    catch       //не существует папка для перемещения, переходим к следующему письму - добавить в уведомление
                     {
-
+                        Console.Write("Message didn't move to \\\\soc@RT.RU\\ELK\\Done.\t");
                     }
+                    Console.WriteLine("Next message.");
                 }
             }
         }
 
-        static void AddToFilterList(List<string> addresses)
+        static void AddToFilterList(List<string> addresses, Browser browser, string login, string pass)
         {
-            IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl("https://vpi1.soc.rt.ru/page?id=mitigation_status&mitigation_id=58640");
-            driver.FindElement(By.Name("username")).SendKeys("a.kucheryavenko");
-            driver.FindElement(By.Name("password")).SendKeys("4_c`&MjLjq");
-            driver.FindElement(By.Name("Submit")).Click();
+            OpenQA.Selenium.Remote.RemoteWebDriver driver;
+            if(browser == 0)
+            {
+                driver = new FirefoxDriver();
+            }
+            else
+            {
+                driver = new ChromeDriver();
+            }
+            
+            driver.Navigate().GoToUrl("https://vpi1.soc.rt.ru/page?id=mitigation_status&mitigation_id=58640");      //здесь и далее в функции может выброситься ex.Source == "WebDriver"
 
-            System.Threading.Thread.Sleep(10000);
+            if (driver.Title.Contains("Login"))
+            {
+                driver.FindElement(By.Name("username")).SendKeys(login);
+                driver.FindElement(By.Name("password")).SendKeys(pass);
+                driver.FindElement(By.Name("Submit")).Click();
+                Thread.Sleep(15000);
+            }
 
             driver.FindElement(By.CssSelector(".alt:nth-child(5) a")).Click();
             IWebElement filterForm = driver.FindElement(By.Name("filter_MitigationRealTimeExpandBWList_bcfea401019cccd2db81b44b4b11d7c9"));
-            string filter = filterForm.Text;
+            string firstFilter = filterForm.Text;
+            string filter = firstFilter;
             foreach (string address in addresses)
             {
                 filter = $"drop src host {address}\r\n{filter}";
@@ -90,8 +154,28 @@ namespace OutlookInboxHandler
             filterForm.SendKeys(filter);
             driver.FindElement(By.CssSelector(".tableheader:nth-child(8) .tick")).Click();
 
-            System.Threading.Thread.Sleep(10000);
+            Thread.Sleep(15000);
+
+            //проверка
+            driver.Navigate().GoToUrl("https://vpi1.soc.rt.ru/page?id=mitigation_status&mitigation_id=58640");
+            driver.FindElement(By.CssSelector(".alt:nth-child(5) a")).Click();
+            filterForm = driver.FindElement(By.Name("filter_MitigationRealTimeExpandBWList_bcfea401019cccd2db81b44b4b11d7c9"));
+            if (filterForm.Text != filter)
+            {
+                if (filterForm.Text != firstFilter)
+                {
+                    filterForm.Clear();
+                    filterForm.SendKeys(firstFilter);
+                    driver.FindElement(By.CssSelector(".tableheader:nth-child(8) .tick")).Click();
+                    Thread.Sleep(15000);
+                }
+                driver.FindElement(By.ClassName("user")).FindElement(By.TagName("a")).Click();
+                driver.Dispose();
+                throw new System.Exception("Arbor ERROR: can't add addreses to filter lists") { Source = "WebDriver" };     //если адреса не добавились
+            }
+
             driver.FindElement(By.ClassName("user")).FindElement(By.TagName("a")).Click();
+            driver.Dispose();
         }
 
         static async Task<bool> TelegramNotification(List<string> addresses)
@@ -118,26 +202,53 @@ namespace OutlookInboxHandler
         {
             try
             {
+                Console.WriteLine($"ELKAddressAdder started at {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")}");
                 List<string> addresses = new List<string>();
+                Browser browser = new Browser();
+
+                if (args.Count() != 3 || args[1] == null || args[2] == null)
+                {
+                    throw new System.Exception("Parameters ERROR: Invalid parameters or number of parameters");
+                }
+                if (String.Compare($"Firefox", args[0], true) == 0)
+                {
+                    browser = Browser.Firefox;
+                }
+                else if(String.Compare($"Chrome", args[0], true) == 0)
+                {
+                    browser = Browser.Chrome;
+                }
+                else
+                {
+                    throw new System.Exception("Parameters ERROR: Invalid first parameter");
+                }
 
                 GetAddressesFromOutlook(ref addresses);
 
                 if (addresses.Any())
                 {
-                    //AddToFilterList(addresses);
+                    AddToFilterList(addresses, browser, args[1], args[2]);
                 }
 
                 bool status = await TelegramNotification(addresses);
             }
             catch(System.Exception ex)
             {
-                if(ex.Source == "mscorlib")     //если закрыт OutLook - уведомить
+                if(ex.Source == "OutlookInboxHandler")      //если ошибка в args
                 {
-
+                    Console.WriteLine($"{ex.Message}\nExiting.");
                 }
-                if (ex.Source == "Microsoft Outlook")        //если неверный путь к папке - уведомить
+                else if(ex.Source == "mscorlib")     //если закрыт OutLook - уведомить
                 {
-
+                    Console.WriteLine("ERROR: Microsoft Outlook isn't running.\nExiting.");
+                }
+                else if (ex.Source == "Microsoft Outlook")        //если неверный путь к папке - уведомить
+                {
+                    Console.WriteLine("ERROR: folder not found.\nExiting.");
+                }
+                else if (ex.Source == "WebDriver")   //если ошибка в работе с Арбор
+                {
+                    Console.WriteLine($"{ex.Message}\nExiting.");
                 }
             }
         }
